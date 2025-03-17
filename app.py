052a49e4ed488f564c78da51e14d8a9e992140c4
Ulrich <macbook@Macbooks-MacBook-Pro.local>
2025-03-17 20:28:53 +0100

    Premier commit - ajout du projet

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+ return "Bonjour à tous , Ceci est une simple application conteneuriser avec Docker parmon_non!"
+
+if __name__ == "__main__":
+ app.run(host='0.0.0.0', port=5000)
